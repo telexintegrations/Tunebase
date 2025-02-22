146b6bc4421b4a67d8ebf38b70d8d20297858c32
Mukeli3 <mukelicynthia20@gmail.com>
2025-02-22 17:13:00 +0000

    feat: added payload

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,20 @@ def send_to_telex(data):
     """
     sends fetched song to telex channel webhook
     """
-    url = Config.TELEX_CHANNEL_WEBHOOK
-    response = requests.post(url, json=data)
+    payload = {
+        "message": str(data), 
+        "event_name": "Trending songs",
+        "status": "success",
+        "username": "Ultra - Music bot "
+    }
+    
+    response = requests.post(
+        Config.TELEX_CHANNEL_WEBHOOK,
+        json=payload,
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    )
+    print(response.json())
     return response.status_code
